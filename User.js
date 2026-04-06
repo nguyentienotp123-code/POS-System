9b242fbfc66d896d6f7c4985da07d04b54aa49e1
nguyentienotp123-code <nguyentien.otp123@gmail.com>
2026-04-06 20:58:12 +0700

    Delete User.js

--- User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    role: { type: String, default: "staff" }
-});
-
-module.exports = mongoose.model("User", UserSchema);
