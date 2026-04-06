f3a4094fa7823a937361b16ebb6a69ec1d2ae2c7
nguyentienotp123-code <nguyentien.otp123@gmail.com>
2026-04-06 20:57:30 +0700

    Delete Table.js

--- Table.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TableSchema = new mongoose.Schema({
-    name: String,
-    status: { type: String, default: "empty" }
-});
-
-module.exports = mongoose.model("Table", TableSchema);
